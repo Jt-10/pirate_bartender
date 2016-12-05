4e4edc57eedda7e16e62bba7d142ec9a0e3dc279
Jt-10 <jonthrp@yahoo.com>
2016-12-05 22:09:59 +0000

    Third commit

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -21,12 +21,6 @@ ingredients = {
 
 preferences = {}
 
-adjectives = ["Arrogant","Bubbly","Comfortable","Dilicious","Earthy","Diabolical"]
-
-nouns = ["Aardvark","Pomeranian","Chinchilla","Swimmer","Herb","Beach"]
-
-drink = []
-
 def obtain_preferences():
     """Gather customer responses to the questions and save them in the
     preferences dictionary."""
@@ -42,15 +36,27 @@ def obtain_preferences():
     
     return preferences
     
-def pour(preferences):
+def pour_drink(preferences):
     """Use the preferences and ingredients dictionaries to randomly select
     ingredients that correspond to flavors the customer enjoys."""
+    
+    drink = []
     
     for flavor in preferences:
         if preferences[flavor] == True:
             drink.append(random.choice(ingredients[flavor]))
     
-    return drink
+    print(drink)
+    
+def name_drink():
+    """This function assigns a name by randomly selecting one adjective and one
+    noun"""
+    
+    adjectives = ["Arrogant","Bubbly","Comfortable","Dilicious","Earthy","Diabolical"]
+    nouns = ["Aardvark","Pomeranian","Chinchilla","Swimmer","Herb","Beach"]
+    
+    name = str(random.choice(adjectives)) + " " + str(random.choice(nouns))
+    print(name)
 
 def main():
     """This function calls the obtain_preferences function and imports the
@@ -61,12 +67,9 @@ def main():
     
     while customer == "sober":
         obtain_preferences()
-        pour(preferences)
-    
-        name = str(random.choice(adjectives)) + " " + str(random.choice(nouns))
+        name_drink()
+        pour_drink(preferences)
         
-        print(name)
-        print(drink)
         
         sobriety = input("Would you like another beverage? ").lower()
         if sobriety == "no":
@@ -74,7 +77,7 @@ def main():
         elif sobriety == "n":
             customer = "drunk"
         else:
-            pass
+            customer == "sober"
         
     
 if __name__ == "__main__":
